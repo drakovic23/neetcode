0b02881f7aa50811c02bb3f1c8e61ca812be3775
Deni Rakovic <deni.rakovic23@gmail.com>
2023-12-01 17:15:14 -0500

    Contains Duplicate

---
+++ nc_containsDuplicate/nc_containsDuplicate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace nc_containsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ nc_containsDuplicate/nc_containsDuplicate/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nc_containsDuplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> integers = new HashSet<int>();
+
+            foreach(int i in nums){
+
+                if(integers.Add(i) == false){
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
